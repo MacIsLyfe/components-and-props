65d6c3b3d8739a24e6e8db14f49de7a4ba9bd8b6
Mac Cooke <101891594+MacIsLyfe@users.noreply.github.com>
2022-10-20 03:47:31 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Holiday from "./Holiday";
+
+function App() {
+  return (
+   <div>
+    <Holiday name="Valentine's Day" day="14" month="February" />
+    <Holiday name="Christmas" day="25" month="December" />
+   </div> 
+  )
+}
+
+export default App;
